a5a853f60035e273c4d9341b1c534fe0748bf8af
swalke16 <swalke16@gmail.com>
2009-09-03 23:41:29 -0400

    added highline for terminal

--- Greed/lib/ui/greed_game_console_ui.rb
+++ Greed/lib/ui/greed_game_console_ui.rb
@@ -1,13 +1,17 @@
 # TODO: create another UI on top
-# TODO: allow default input to be "y" without blank line
-# TODO: fix overwrite issues on display
-# TODO: better display of player status after every roll
+# TODO: failing specs... 
+# TODO: is there a way to cleanup colors better so strings aren't so long?
+# TODO: notification of points not accumulating because of less < 300
+
+require 'rubygems'
+require 'highline'
 
 class GreedGameConsoleUI
 
   def initialize(greed_game_engine)
     raise ArgumentError, "Game engine cannot be nil!" if greed_game_engine == nil
     @game_engine = greed_game_engine
+    @terminal = HighLine.new
   end
 
   def play()
@@ -18,18 +22,18 @@ class GreedGameConsoleUI
       @game_engine.play(self, players)
       display_game_summary()
     rescue ArgumentError => error
-      write_line("")
-      write_line(error)
+      @terminal.say("\n")
+      @terminal.say(error)
     end      
   end
 
-  def turn_starting(turn)
-    display_turn_status(turn)
-  end
-
   def player_wants_to_roll_again?(turn)
-    write "You have #{turn.dice_left_to_roll} dice left to roll. Roll dice? ((y)/n): "
-    get_input.chomp.downcase == "y"
+    @terminal.agree(%{<%= color("#{turn.player.name}'s", :yellow) %> turn! In Game: <%= color('#{turn.player.in_game}', :yellow) %> Game Score: <%= color('#{turn.player.score}', :yellow) %> Turn Score: <%= color('#{turn.score}', :yellow) %>
+You have <%= color('#{turn.dice_left_to_roll}', :cyan) %> dice left to roll. Roll dice? <%= color('(y/n)', :cyan) %> }, true) do |q|
+        q.overwrite = true
+        q.echo      = false  # overwrite works best when echo is false.
+        q.character = true   # if this is set to :getc then overwrite does not work
+    end    
   end
 
   def turn_ending(turn)
@@ -41,80 +45,59 @@ class GreedGameConsoleUI
   end
 
   def entering_final_round(leading_player)
-    write_line %{
-#{leading_player.name} has scored greater than 3000 points!
-The game is now entering the final round in which all players except #{leading_player.name} will have one final turn.
-    }
+    @terminal.say(%{
+<%= color('#{leading_player.name}', :yellow) %> has scored greater than 3000 points!
+The game is now entering the final round in which all players except <%= color('#{leading_player.name}', :yellow) %> will have one final turn.})
+    @terminal.say("\n")
   end
 
 private
   def display_game_info()
-    write_line ""
-    write_line "Welcome to the game of Greed!"
-    write_line ""
+    @terminal.say("\n")
+    @terminal.say("Welcome to the game of <%= color('Greed', :green) %>!")
+    @terminal.say("\n")
   end
 
   def prompt_for_players()
-    write_line "Please enter player names, or a blank name to stop creating players."
+    @terminal.say("Please enter player names, or a blank name to stop creating players.")
 
     players = []
     player_name = nil
-    while player_name != ""
-      write %{Enter player #{players.length + 1} name: }
-      player_name = get_input()
-      players << player_name unless player_name == ""
+    begin
+      while player_name != ""
+        player_name = @terminal.ask("Enter player <%= color('#{players.length + 1}', :yellow) %> name: ")
+        players << player_name unless player_name == ""
+      end
+    rescue EOFError
     end
 
-    write_line ""
+    @terminal.say("\n")
 
     return players.map {|name| Player.new(name)}
   end
 
   def display_lost_turn()
-    write_line "You rolled a zero so you lost your turn and any accumulated score for this turn."
-  end
-
-  def display_turn_status(turn)
-    write_with_overwrite "#{turn.player.name}'s turn! In Game: #{turn.player.in_game} Game Score: #{turn.player.score} Turn Score: #{turn.score}\n"
+    @terminal.say("<%= color('You rolled a zero so you lost your turn and any accumulated score for this turn.', :red) %>")
+    @terminal.say("\n")
   end
 
   def display_turn_ending(turn)
-    write_line "Your turn has now ended! You scored a total of #{turn.score} points for this turn!"
-    write_line ""
+    @terminal.say("Your turn has now ended! You scored a total of <%= color('#{turn.score}', :green) %> points for this turn!")
+    @terminal.say("\n")
   end
 
   def display_game_summary()
-    write_line ""
+    @terminal.say("The game has now ended!")
 
-    # display game summary info
-    write_line "The game has now ended!"
-
-    # should we move some of this logic inside the game engine?
     winning_player = @game_engine.players.inject {|lead, player| player.score > lead.score ? player : lead}
     summary = @game_engine.players.inject("Game Summary:\n") do |text, player|
-       text << "#{player.name} scored: #{player.score} points."
-       text << " -- WINNER!" if player == winning_player
+       text << "<%= color('#{player.name}', :yellow) %> scored: <%= color('#{player.score}', :yellow) %> points."
+       text << "<%= BLINK %><%= color(' <-- WINNER!', :magenta) %><%= CLEAR %>" if player == winning_player
        text << "\n"
     end
     
-    write_line summary
+    @terminal.say(summary)
+    @terminal.say("\n")
   end
 
-  def get_input
-    !$stdin.eof? ? $stdin.readline.chomp : ""
-  end
-
-  def write(output)
-    $stdout.write output
-  end
-
-  def write_line(output)
-    $stdout.puts output
-  end
-
-  def write_with_overwrite(output, &block)
-    $stdout.write output
-    yield if block_given?
-    $stdout.write "\b" * output.length;
-  end
 end
--- Greed/spec/ui/greed_game_console_ui_spec.rb
+++ Greed/spec/ui/greed_game_console_ui_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + "/../spec_helper"
 describe GreedGameConsoleUI do
   context "when first created" do
     it "is created" do
-      game_ui = GreedGameConsoleUI.new( flexmock("fake game engine", :input_request_handler= => nil) )
+      game_ui = GreedGameConsoleUI.new( flexmock("fake game engine") )
       game_ui.should_not be_nil
     end
 
@@ -13,13 +13,11 @@ describe GreedGameConsoleUI do
   end
   
   context "when playing" do
-    before(:each) do
-      @players = ["tom", "dick", "harry"]
-      @output = StringIO.new
-      @input = StringIO.new(@players.join("\n"), "r")
+    before(:each) do            
+      @terminal = flexmock("fake terminal") 
 
-      $stdout = @output
-      $stdin = @input
+      @terminal.should_receive(:ask).times(4).with(/Enter player/).and_return("tom", "dick", "harry", "")
+      flexmock(HighLine).should_receive(:new).and_return(@terminal)
 
       @game_engine = flexmock("fake game engine")
       @game_ui = GreedGameConsoleUI.new(@game_engine)
@@ -27,16 +25,14 @@ describe GreedGameConsoleUI do
 
     it "prompts for player names until no names entered" do
       @game_engine.should_ignore_missing
+      @terminal.should_ignore_missing
 
       @game_ui.play()
-
-      @players.each_index do |idx|
-         @output.string.should match(%{Enter player #{idx + 1} name: })
-      end
     end
 
     it "plays the game with the players that were entered"  do
       @game_engine.should_ignore_missing
+      @terminal.should_ignore_missing
       @game_engine.
         should_receive(:play).
         with(@game_ui, FlexMock.on {|arg| arg[0].name == "tom" && arg[1].name == "dick" && arg[2].name == "harry" })
@@ -46,10 +42,10 @@ describe GreedGameConsoleUI do
 
     it "writes ArgumentErrors from the game engine to the console" do
       @game_engine.should_receive(:play).and_raise(ArgumentError, "fake argument error")
+      @terminal.should_receive(:say).at_least.once.with(/fake argument error/)
+      @terminal.should_receive(:say)
 
       @game_ui.play()
-
-      @output.string.should match(/fake argument error/)
     end
   
     it "prints score for every player and indicate winning player" do
@@ -58,42 +54,18 @@ describe GreedGameConsoleUI do
       harry = flexmock("player tom", :name=>"harry", :score=>0)        
       @game_engine.should_ignore_missing
       @game_engine.should_receive(:players).and_return([tom, dick, harry])
+      
+      @terminal.should_receive(:say).at_least.once.with(/tom.*3001.*WINNER.*dick.*0.*harry.*0/m)
+      @terminal.should_receive(:say)
 
-      @game_ui.play()
-
-      @output.string.should match(/tom scored: 3001 points. -- WINNER!/)
-      @output.string.should match(/dick scored: 0 points./)
-      @output.string.should match(/harry scored: 0 points./)    
-    end
-  end
-
-  context "when a turn is starting for a player named tom" do
-    before(:each) do
-      @output = StringIO.new
-      $stdout = @output
-
-      @game_engine = flexmock("fake game engine")
-      @game_ui = GreedGameConsoleUI.new(@game_engine)
-    end
-  
-    it "displays the status of the turn" do
-      turn = flexmock("fake turn")
-      turn.should_receive("player.name").and_return("tom")
-      turn.should_receive("player.in_game").and_return(true)
-      turn.should_receive("player.score").and_return(3001)
-      turn.should_receive("score").and_return(500)
-    
-      @game_ui.turn_starting(turn)
-    
-      @output.string.should match(/tom's turn! In Game: true Game Score: 3001 Turn Score: 500/)
+      @game_ui.play()    
     end
   end
 
   context "when a turn is ending for a player named tom" do
     before(:each) do
-      @output = StringIO.new
-
-      $stdout = @output
+      @terminal = flexmock("fake terminal") 
+      flexmock(HighLine).should_receive(:new).and_return(@terminal)
 
       @game_engine = flexmock("fake game engine")
       @game_ui = GreedGameConsoleUI.new(@game_engine)
@@ -104,51 +76,49 @@ describe GreedGameConsoleUI do
       turn.should_receive("was_lost").and_return(false)
       turn.should_receive("score").and_return(500)
     
-      @game_ui.turn_ending(turn)
+      @terminal.should_receive(:say).with(/Your turn has now ended! You scored a total of.*500/)
+      @terminal.should_receive(:say)
     
-      @output.string.should match(/Your turn has now ended! You scored a total of 500 points for this turn!/)
+      @game_ui.turn_ending(turn)
     end
   
     it "displays the lost turn message if the turn was lost" do
       turn = flexmock("fake turn")
       turn.should_receive("was_lost").and_return(true)
     
-      @game_ui.turn_ending(turn)
+      @terminal.should_receive(:say).with(/You rolled a zero so you lost your turn and any accumulated score for this turn./)
+      @terminal.should_receive(:say)
     
-      @output.string.should match(/You rolled a zero so you lost your turn and any accumulated score for this turn./)    
+      @game_ui.turn_ending(turn)
     end  
   end
 
   context "when asking if a player wants to roll again" do
     before(:each) do
-      @output = StringIO.new
-      $stdout = @output
+      @terminal = flexmock("fake terminal") 
+      flexmock(HighLine).should_receive(:new).and_return(@terminal)
+
+      @turn = flexmock("fake turn")
+      @turn.should_receive("dice_left_to_roll").and_return(3)
+      @turn.should_ignore_missing
 
       @game_engine = flexmock("fake game engine")
       @game_ui = GreedGameConsoleUI.new(@game_engine)
     end
 
     it "asks player if they want to roll again, returning true if the player typed a 'y'" do
-      turn = flexmock("fake turn")
-      turn.should_receive("dice_left_to_roll").and_return(3)
-      input = StringIO.new("y")
-      $stdin = input
+      @terminal.should_receive(:agree).with(/You have.*3.*dice left to roll. Roll dice?/, true, Proc).and_return(true)
     
-      roll_again = @game_ui.player_wants_to_roll_again?(turn)    
+      roll_again = @game_ui.player_wants_to_roll_again?(@turn)    
     
-      @output.string.should match(/You have 3 dice left to roll. Roll dice?/)
       roll_again.should eql(true)
     end
 
     it "asks player if they want to roll again, returning false if the player typed a 'n'" do
-      turn = flexmock("fake turn")
-      turn.should_receive("dice_left_to_roll").and_return(3)
-      input = StringIO.new("n")
-      $stdin = input
+      @terminal.should_receive(:agree).with(/You have.*3.*dice left to roll. Roll dice?/, true, Proc).and_return(false)
     
-      roll_again = @game_ui.player_wants_to_roll_again?(turn)    
+      roll_again = @game_ui.player_wants_to_roll_again?(@turn)    
     
-      @output.string.should match(/You have 3 dice left to roll. Roll dice?/)
       roll_again.should eql(false)
     end
   end
